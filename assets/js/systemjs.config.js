dda2c6cbba22fd9965e3b64f8a21637eff8e4c43
Eduardo Pineda <sepineda@gmail.com>
2017-09-10 17:42:14 -0600

    Cleanup

--- assets/js/systemjs.config.js
+++ assets/js/systemjs.config.js
@@ -9,10 +9,8 @@
     '@angular': 'node_modules/@angular',
     'angular-in-memory-web-api': 'node_modules/angular-in-memory-web-api',
     'rxjs': 'node_modules/rxjs',
-    'angular2-materialize': 'node_modules/angular2-materialize',
     'plugin-babel': 'plugin-babel.js',
-    'systemjs-babel-build': 'systemjs-babel-browser.js',
-    'materialize-css': 'node_modules/materialize-css'
+    'systemjs-babel-build': 'systemjs-babel-browser.js'
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
@@ -30,15 +28,6 @@
     },
     'angular-in-memory-web-api': {
       main: 'index.js',
-      defaultExtension: 'js'
-    },
-    'angular2-materialize': {
-      main: 'dist/index',
-      defaultExtension: 'js'
-    },
-    "materialize-css": {
-      format: "global",
-      main: "dist/js/materialize",
       defaultExtension: 'js'
     }
   };
