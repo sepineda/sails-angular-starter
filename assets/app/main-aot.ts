f91582f04481db2a446164bc926b6f757acd4d8e
Eduardo Pineda <eduardo.pineda@hpe.com>
2017-11-06 09:19:51 -0600

    Moving to Angular CLI

--- assets/app/main-aot.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from './aot/assets/app/app.module.ngfactory';
-
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
