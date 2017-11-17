69e96773d70bce1224d8cc18f4279ff21a7563e5
Eduardo Pineda <eduardo.pineda@hpe.com>
2017-11-17 11:06:39 -0600

    Angular cli ng serve upgrade

--- config/express.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-module.exports.http = {
-customMiddleware: function (app) {
-    app.use('/node_modules', express.static(process.cwd() + '/node_modules'));
-   }
-};
